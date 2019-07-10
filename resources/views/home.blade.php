dfd385af1e8a8ea43b5db977496aa03a0cc19253
muhammadfahrurozi <muhamad.fahrurozi@koding.sch.id>
2019-07-10 21:00:04 +0700

    belajar3

---
+++ resources/views/home.blade.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <nav class=" fixed-top navbar navbar-expand-lg navbar-light bg-danger">
+  <img src="https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/0_qths2ajr/def_height/2700/def_width/2700/version/100012/type/1"height="20">
+  <a class="navbar-brand" href="/home">home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/about">about <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/contact">contact</a>
+      </li>
+     
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="https://i.ytimg.com/vi/dIA0yKove2k/maxresdefault.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.mumslounge.com.au/wp-content/uploads/2016/08/colonels-original-recipe-KFC-06.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.ytimg.com/vi/h8GnGsbssn8/maxresdefault.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="https://d30v2pzvrfyzpo.cloudfront.net/images/chains/kfc-opengraph-1.jpg" class="card-img-top" alt="..."height="175">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example KFC Chiken - I Wash There.</p>
+    <a href="#" class="btn btn-danger">beli - $=....</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="https://www.visitstaugustine.com/sites/default/files/todo/img/KentuckyFriedChickenFood_1.jpg" class="card-img-top" alt="..."height="175">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example Kentucky Flatbread Spicy Chiken Box Meal.</p>
+    <a href="#" class="btn btn-danger">beli - $=....</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="https://www.gannett-cdn.com/presto/2018/11/12/USAT/47b10996-6ed6-476d-9a87-f8af66fe0464-Kentucky_Fried_Chicken_n_Waffles.jpg?crop=1711,962,x169,y634&width=3200&height=1680&fit=bounds" class="card-img-top" alt="..."height="175">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example Chiken&Waffles.</p>
+    <a href="#" class="btn btn-danger">beli - $=....</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="https://im1.dineout.co.in/images/uploads/restaurant/sharpen/7/j/c/p7-1493973566590c3a3eeb783.jpg?w=1200" class="card-img-top" alt="..."height="175">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example KFC,Berasat,Kolkata | Dineout Discovery.</p>
+    <a href="#" class="btn btn-danger">beli - $=....</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="http://pinchofnom.com/wp-content/uploads/2018/11/KFC-Rice-Box-Featured.jpg" class="card-img-top" alt="..."height="175">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example KFC Rice Box | Sliming World & Weight W.atchers Friendly-Pinch Of Nom</p>
+    <a href="#" class="btn btn-danger">beli - $=....</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="https://im1.dineout.co.in/images/uploads/restaurant/sharpen/6/l/r/p6-1493973230590c38eea73c0.jpg?w=1200" alt="..."height="175">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example KFC Anna Nagar, Nort Chenai,Chennai | Dineout Discovery.</p>
+    <a href="#" class="btn btn-danger">beli - $=....</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
